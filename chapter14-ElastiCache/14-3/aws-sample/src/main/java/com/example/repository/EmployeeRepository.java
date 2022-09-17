e0bcfce8a1de212f4a1a60687cbd444f1e58ab94
TatsuyaTamura <34834257+TatsuyaTamura@users.noreply.github.com>
2022-09-17 21:51:44 +0900

    Delete chapter14-ElastiCache directory

--- chapter14-ElastiCache/14-3/aws-sample/src/main/java/com/example/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.domain.employee.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, String> {
-
-}
--- chapter14-ElastiCache/14-3/aws-sample/src/main/resources/schema/data-h2.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO employee (id, name, age)
-VALUES('1', 'local1', 20),
-('2', 'local2', 30),
-('3', 'local3', 40);
